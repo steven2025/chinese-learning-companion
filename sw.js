df9bdfdb6b74f1b953d7bd2eaf2938867b0dcb1b
steven2025 <127971260+steven2025@users.noreply.github.com>
2026-08-26 23:13:10 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "diandian-chinese-v93";
+const CACHE_VERSION = "diandian-chinese-v94";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -34,9 +34,9 @@ const APP_SHELL = [
   "./modules/hanzi-challenge/index.html",
   "./modules/pinyin-generator/index.html",
   "./modules/digital-book/index.html",
-  "./modules/digital-book/styles.css?v=20260826-9",
+  "./modules/digital-book/styles.css?v=20260826-10",
   "./modules/digital-book/pronunciation.css?v=20260824-1",
-  "./modules/digital-book/app.js?v=20260826-9",
+  "./modules/digital-book/app.js?v=20260826-10",
   "./modules/digital-book/pronunciation.js?v=20260824-1"
 ];
 
