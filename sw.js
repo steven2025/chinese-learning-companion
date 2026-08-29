5be9526c66cecf93f5946207e5ad5341d6181f08
steven2025 <127971260+steven2025@users.noreply.github.com>
2026-08-29 08:03:46 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -38,7 +38,7 @@ const APP_SHELL = [
   "./modules/pinyin-generator/index.html",
   "./modules/digital-book/index.html",
   "./modules/digital-book/styles.css?v=20260828-2",
-  "./modules/digital-book/pronunciation.css?v=20260828-8",
+  "./modules/digital-book/pronunciation.css?v=20260828-9",
   "./modules/digital-book/app.js?v=20260828-1",
   "./modules/digital-book/pronunciation.js?v=20260824-1"
 ];
