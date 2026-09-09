1dc9b5ced06deb2921184f738140dd5908686ec0
steven2025 <127971260+steven2025@users.noreply.github.com>
2026-09-09 13:54:34 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "diandian-chinese-v114";
+const CACHE_VERSION = "diandian-chinese-v114-pinyin-assessment-1";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -40,7 +40,7 @@ const APP_SHELL = [
   "./modules/digital-book/styles.css?v=20260828-2",
   "./modules/digital-book/pronunciation.css?v=20260829-2",
   "./modules/digital-book/app.js?v=20260828-1",
-  "./modules/digital-book/pronunciation.js?v=20260824-1"
+  "./modules/digital-book/pronunciation.js?v=20260909-assessment-1"
 ];
 
 self.addEventListener("install", event => {
